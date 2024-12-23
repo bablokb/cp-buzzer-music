3c9706cf26a3bcff385f32788b2fa7ea217d4d85
Bernhard Bablok <bablokb@gmx.de>
2024-12-23 09:44:08 +0100

    simplified

--- examples/simple_buzzer.py
+++ examples/simple_buzzer.py
@@ -19,20 +19,19 @@ from buzzer_music.async_buzzer import AsyncBuzzer
 # notes (pitch,duration,volume),...
 NOTES = [('C4',1,3),('E4',1,7),('G4',1,8),('C5',1.5,10)]
 
+# Sequentially create and wait for task
 async def main1():
   buzzer = AsyncBuzzer(board.GP18)
   for note in NOTES:
     t = asyncio.create_task(buzzer.play(*note))
-    asyncio.run_until_complete(t)
+    await t
   buzzer.deinit()
 
+# Create and start all tasks at once.
+# Will be serialized using a lock within buzzer.play()
 async def main2():
   buzzer = AsyncBuzzer(board.GP18)
-  n_tasks = []
-  for note in NOTES:
-    t = asyncio.create_task(buzzer.play(*note))
-    n_tasks.append(t)
-  await asyncio.gather(*n_tasks)
+  await asyncio.gather(*[buzzer.play(*note) for note in NOTES])
   buzzer.deinit()
 
 asyncio.run(main1())
